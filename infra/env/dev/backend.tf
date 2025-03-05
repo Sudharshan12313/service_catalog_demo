c736deee99f305405eccc826bfa75861c2845d42
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-03-05 11:11:13 +0530

    Update backend.tf

--- infra/env/dev/backend.tf
+++ infra/env/dev/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0.0"  # Replace with the version you want to require
   backend "s3" {
     bucket         = "nsh-state-new"  
     key            = "Service_catalog_state/development/terraform.tfstate"
