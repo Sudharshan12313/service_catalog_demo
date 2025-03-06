0c0218223da2308e304455872dc324699dda3e5a
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-03-06 10:21:31 +0530

    Update main.tf

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -5,11 +5,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-<<<<<<< HEAD
   required_version = ">= 1.5.5"
-=======
-  required_version = ">= 1.3.0"
->>>>>>> 3345401ca8d0bb1dcfdf0d071bd61117a2933e8f
 }
  
 
